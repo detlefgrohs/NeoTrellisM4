b84b0f69e3f006bf3960a9cbb0cf05f344a2aa14
Unknown <detlef.grohs@gmail.com>
2019-01-01 19:01:02 -0500

    TerminalConnect added

---
+++ TerminalConnect.ps1
@@ -0,0 +1,18 @@
+﻿
+
+#$COM = [System.IO.Ports.SerialPort]::getportnames()
+
+#function read-com {
+    $port= new-Object System.IO.Ports.SerialPort COM5,115200,None,8,one
+    $port.Open()
+    $port.DtrEnable = $true
+
+    #Start-Sleep 2 # wait 2 seconds until Arduino is ready
+
+
+    do {
+        $line = $port.ReadLine()
+        Write-Host $line # Do stuff here
+    }
+    while ($port.IsOpen)
+#}
