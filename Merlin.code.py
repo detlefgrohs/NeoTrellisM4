14658041d54f81fee1fd0e3810f19c71c3946db6
Detlef Grohs <detlef.grohs@gmail.com>
2024-08-10 01:42:53 -0400

    Latest changes...

--- Merlin.code.py
+++ Merlin.code.py
@@ -1,26 +1,22 @@
 import math
 import time
 import array
-import board
-import busio
-import audioio
+#import board
+#import busio
+#import audioio
 import adafruit_trellis_express
-import adafruit_adxl34x
+import random
 
-time.sleep(10.0)
+time.sleep(10.0) # Delay on startup...
 
 trellis = adafruit_trellis_express.TrellisM4Express(rotation=90)
 
-i2c = busio.I2C(board.ACCELEROMETER_SCL, board.ACCELEROMETER_SDA)
-accelerometer = adafruit_adxl34x.ADXL345(i2c)
-
-print('Starting up...')
-
 # Clear all pixels
-trellis.pixels._neopixel.fill((0, 0, 0))
-trellis.pixels._neopixel.show()
+fillTrellis(trellis, (0, 0, 0))
 
-import random
+def fillTrellis(trellis,fillColor):
+    trellis.pixels._neopixel.fill(fillColor)
+    trellis.pixels._neopixel.show()
 
 class LightsOutBoard:
     def __init__(self, xSize, ySize, initialValue = False):
@@ -29,17 +25,6 @@ class LightsOutBoard:
         self.Board = [ [ initialValue for y in range(ySize) ] for x in range(xSize) ]
         self.lightOffColor = (0, 0, 0)
         self.lightOnColor = (255, 255, 255)
-
-    def displayBoard(self):
-        print("Board: %s" % ("Won" if self.hasWon() else "!Won"))
-        for y in range(self.ySize):
-            line = "%d: " % y
-            for x in range(self.xSize):
-                if self.Board[x][y]:
-                    line = line + "X"
-                else:
-                    line = line + " "
-            print(line)
 
     def generateRandomBoard(self):
         for x in range(self.xSize):
@@ -54,11 +39,8 @@ class LightsOutBoard:
                     count = count + 1
         return count
 
-    def hasWon(self, allSet = False):
-        count = self.countOfSetCells()
-        if count == 0 and allSet == False:
-            return True
-        if count == (self.xSize * self.ySize) and allSet == True:
+    def hasWon(self):
+        if self.countOfSetCells() == 0:
             return True
         return False
 
@@ -75,44 +57,32 @@ class LightsOutBoard:
         for x in range(self.xSize):
             for y in range(self.ySize):
                 trellis.pixels[(y, x)] = self.lightOnColor if self.Board[x][y] else self.lightOffColor
-        #trellis.pixels._neopixel.show()
 
 board = LightsOutBoard(8, 4)
 
 while True:
-
-    board.generateRandomBoard()
+    board.generateRandomBoard()         # Setup board
     board.updateTrellis(trellis)
-    board.displayBoard()
 
     last_pressed = set()
 
     while board.hasWon() == False:
         stamp = time.monotonic()
-
         pressed = set(trellis.pressed_keys)
 
         for down in pressed - last_pressed:
-            print("Pressed down", down)
-
-            y = down[0]
-            x = down[1]
-
-            board.toggleCell(x, y)
-
+            board.toggleCell(down[1], down[0])
             board.updateTrellis(trellis)
-            board.displayBoard()
 
         last_pressed = pressed
 
         while time.monotonic() - stamp < 0.1:
             time.sleep(0.01)  # a little delay here helps avoid debounce annoyances
 
+    # Player has won!!!
     for count in range(5):
-        trellis.pixels._neopixel.fill((255, 0, 0))
-        trellis.pixels._neopixel.show()
+        fillTrellis(trellis, (255, 0, 0))
         time.sleep(0.5)
 
-        trellis.pixels._neopixel.fill((0, 0, 0))
-        trellis.pixels._neopixel.show()
+        fillTrellis(trellis, (0, 0, 0))
         time.sleep(0.5)
